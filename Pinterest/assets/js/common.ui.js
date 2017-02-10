4727ef739979883352652306a05654780aa9aa90
Lee JongSeok(쫑) <lee_jongseok01@eland.co.kr>
2017-02-10 14:34:12 +0900

    pinterest (반응형) Filter 포함작업

--- Pinterest/assets/js/common.ui.js
+++ Pinterest/assets/js/common.ui.js
@@ -1,8 +1,41 @@
 /* Common Ui Script */
+$(function(){
 
-window.onload = function () {
-    var wookmark1 = new Wookmark('#wookmark1', {
-        outerOffset: 10, 
-        itemWidth: 210 
+    $('#tiles').imagesLoaded(function() {
+        
+            var options = {
+                align: 'center',
+                autoResize: true, 
+                offset: 10, 
+                itemWidth: 263, 
+                fillEmptySpace: false 
+            };			
+
+            var $filters = $('#filters li');
+            var handler = new Wookmark('#tiles',options);
+            
+            $filters.on("click", function(event){
+
+                var $item = $(event.currentTarget);
+                var filterType = $item.data('filter');
+				var activeFilters = [];
+                        
+                if (filterType === 'all') {
+                    $filters.removeClass('active');
+                } else {
+                    $item.toggleClass('active');
+                }
+				
+				$filters.filter('.active').each(function() {
+                   activeFilters.push($(this).data('filter'));
+				});
+	
+				handler.filter(activeFilters, 'or');
+
+            });
+                
     });
-};
+
+});
+
+
